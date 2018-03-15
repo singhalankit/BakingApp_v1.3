3dd4741c386449ee48646604eee0e044fcf306c4
Ankit Singhal <ankitsinghal.mnnit@gmail.com>
2018-03-15 12:21:36 +0530

    fixing widget

--- app/src/main/java/com/example/ankit_pc/bakingappudacity/Widget_Config.java
+++ app/src/main/java/com/example/ankit_pc/bakingappudacity/Widget_Config.java
@@ -1,14 +1,19 @@
 package com.example.ankit_pc.bakingappudacity;
 
 import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Widget_Config extends AppCompatActivity {
+
+    Context context ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,17 +92,35 @@ public class Widget_Config extends AppCompatActivity {
             editor.putString("recipe_name",recipe);
             editor.commit();
 
+
+
+
+            Intent result = new Intent(getApplication().getApplicationContext(), RecipeWidgetService.class);
+            result.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            // Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
+            // since it seems the onUpdate() is only fired on that:
+            AppWidgetManager widgetManager = AppWidgetManager.getInstance(getApplication());
+            int[] ids = widgetManager.getAppWidgetIds(new ComponentName(getApplication(), RecipeWidgetService.class));
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+                widgetManager.notifyAppWidgetViewDataChanged(ids, android.R.id.list);
+
+            result.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+            getApplication().sendBroadcast(result);
+
+
+
 //Create the return intent//
 
-            Intent resultValue = new Intent();
+         //   Intent resultValue = new Intent();
 
 //Pass the original appWidgetId//
 
-            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+           // resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
 
 //Set the results from the configuration Activity//
 
-            setResult(RESULT_OK, resultValue);
+//            setResult(RESULT_OK, resultValue);
 
 //Finish the Activity//
 
